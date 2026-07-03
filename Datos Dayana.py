8ea5686ec8fa8d55f3c6d5bcf47d825a42d106f4
Dayana6d <dayana.rivera2028@hilasal.superate.org>
2026-07-02 20:13:30 -0600

    Datos Dayana

---
+++ Datos Dayana.py
@@ -0,0 +1,25 @@
+nombre = "Dayana Gisell Rivera Ramirez"
+edad = "15 años"
+nacionalidad = "Salvadoreña"
+idioma = "Español"
+estatura = "1.55 m"    
+comida_favorita = "Pescado"    
+color_favorito = "Azul"
+carrera = "Negocios Internacionales"
+pasatiempo = "Escribir poesía y libros"
+meta = "Crear una carrera internacional en negocios y tecnología"
+ 
+ 
+print(f"Nombre completo : {nombre}")
+print(f"Edad            : {edad}")
+print(f"Nacionalidad    : {nacionalidad}")
+print(f"Idioma natal    : {idioma}")
+print(f"Estatura        : {estatura}")
+print(f"Color favorito  : {color_favorito}")
+print(f"Comida favorita : {comida_favorita}")
+print(f"Carrera soñada  : {carrera}")
+print(f"Pasatiempo      : {pasatiempo}")
+print(f"Meta principal  : {meta}")
+ 
+print("thank you for see my code")
+print("wooooooow")
